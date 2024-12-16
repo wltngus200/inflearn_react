6ec611a133cd2e7408ec5b64fbeb4a2c37d3ee80
home <wltngus200@gmail.com>
2024-12-16 23:57:30 +0900

    1216 Node.js

---
+++ section04/chapter02.js
@@ -0,0 +1,11 @@
+// Node.js 설치
+
+// Node를 이용해 자바스크립트를 실행하도록 개발환경 설정
+// Node.js는 프로그램 (언어, 라이브러리 X)
+
+// https://nodejs.org/ko 접속 -> LTS~Long Term Support(현재 가장 안정적인 버전)
+// 명령 프롬프트에 node -v 를 입력해 버전 확인 20이상의 짝수면 문제 X
+
+// NPM(Node Package Manager) 설치 확인
+// -Node.js의 프로젝트 단위인 패키지 관리 도구- ex. 새 패키지 생성, 외부라이브러리 설치, 삭제
+// 명령 프롬프트에서 npm -v 를 입력해 버전 확인
