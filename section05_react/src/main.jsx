2a418babe987b4ea857b18d3aaf47f14f31675f7
home <wltngus200@gmail.com>
2024-12-17 22:14:46 +0900

    1217 react 구동원리

--- section05_react/src/main.jsx
+++ section05_react/src/main.jsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// 리액트가 제공하는 내부 메소드
+// createRoot는 인수로 전달 받은 html 요소를 루트로 만듦
 createRoot(document.getElementById('root')).render(
+  // render 메소드를 통해 루트 아래에 렌더링 -> App
   <StrictMode>
+    {/* 컴포넌트를 렌더링하는 문법 -> ES Module 시스템으로 import시 jsx파일에서 HTML tag 들을 반환*/}
     <App />
   </StrictMode>,
 )
