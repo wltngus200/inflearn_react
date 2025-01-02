93ce166c3ea164b133f58045f0842a005691c810
home <wltngus200@gmail.com>
2025-01-02 17:08:28 +0900

    0102 이벤트 핸들링

--- section06_react/src/components/Button.jsx
+++ section06_react/src/components/Button.jsx
@@ -9,11 +9,23 @@ const Button =(props)=>{
 // 객체가 들어오는 것을 알기 때문에 구조분해 할당을 사용
 // HTML 태그를 Props로 전달
 const Button =({text, color, children})=>{
-    return <button style={{color:color}}>{text}-{color.toUpperCase()}-{children}</button>
+    // 이벤트 핸들러에서 설정될 함수가 길 경우
+    // 이벤트 객체 리액트에서 발생하는 모든 이벤트는 이벤트 핸들러 함수를 호출하며 매개변수로 이벤트 객체 제공
+    const onClickButton=(e)=>{ // e : 합성 이벤트 객체
+        console.log(e);
+        console.log(text);
+    }
+    return <button 
+    // 이벤트 핸들러
+    // onClick={()=>{console.log(text)}}
+    onClick={onClickButton} // 함수의 결과를 전달 X 함수 자체를 전달
+    // onMouseEnter={onClickButton} // 마우스를 올렸을 경우 발생
+    style={{color:color}}>{text}-{color.toUpperCase()}-{children}</button>
 }
 
 // 오류가 발생하지 않도록 디폴트값 설정
 Button.defaultProps={color:"black"}
+
 
 // 내보내기 
 export default Button;
